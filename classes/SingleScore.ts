e33584bb9459f47cd03d49d2f0a34abbdece3eb6
fahmidareem3 <fahmidareem3@gmail.com>
2023-10-04 03:36:35 +0600

    feat(score): implementation of scoring

---
+++ classes/SingleScore.ts
@@ -0,0 +1,15 @@
+import { Scorable } from '../interfaces/Scorable';
+
+export class SingleScore implements Scorable {
+  private achievements: number;
+  private timeTaken: number;
+
+  constructor(achievements: number, timeTaken: number) {
+    this.achievements = achievements;  // For instance, the number of achievements or tasks completed by the player
+    this.timeTaken = timeTaken;        // Amount of time taken to complete the game or reach a certain point
+  }
+
+  getScore(): number {
+    return this.achievements - this.timeTaken * 0.5; // As an example, each achievement adds a point while time taken deducts points
+  }
+}
---
+++ interfaces/Scorable.ts
@@ -0,0 +1,3 @@
+export interface Scorable {
+    getScore(): number;
+  }
