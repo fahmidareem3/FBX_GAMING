35f4c038d010cdb9e107bba3e7a582a810ad3a43
fahmidareem3 <fahmidareem3@gmail.com>
2023-10-04 04:55:49 +0600

    feat(auth) : addition of auth

---
+++ src/classes/Auth.ts
@@ -0,0 +1,16 @@
+// models/ConcreteUser.ts
+import { User } from '../abstracts/User';
+
+export class Auth extends User {
+    login(): void {
+        console.log(`${this.user_name} has logged in.`);
+    }
+
+    logout(): void {
+        console.log(`${this.user_name} has logged out.`);
+    }
+
+    register(): void {
+        console.log(`${this.user_name} has registered.`);
+    }
+}
